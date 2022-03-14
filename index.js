890276aa451748ffb1796bf1414a6183780b31ec
+ <+>
2022-03-14 11:48:39 -0400

    commit tree e592767ea3cf61b9821eb950e32fc46db0ba2cad

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says meow!`;
+  }
+}
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says woof!`;
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    let result
+    if (this.sex === "male") result = `It's me! ${this.name}, the parrot!`;
+    else {
+      result = `${this.name} says squawk!`;
+    }
+    return result;
+  }
+}
